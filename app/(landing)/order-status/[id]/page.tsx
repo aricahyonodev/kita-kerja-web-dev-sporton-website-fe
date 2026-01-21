5218b6390c5f1b77be626a1096dc5d074d468052
aricahyonodev <cahyono.ari81@gmail.com>
2026-01-21 10:44:09 +0700

    feat<OrderStatus>: add logic check status transaction

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -1,18 +1,25 @@
-"use client";
-
-import { useState } from "react";
 import OrderConfirmed from "../../components/order-status/order-confirmed";
+import OrderRejected from "../../components/order-status/order-rejected";
 import OrderSubmitted from "../../components/order-status/order-submitted";
+import { getTransactionById } from "@/app/services/transaction.service";
 
-const OrderStatus = () => {
-  const [isConfirmed, setIsConfirmed] = useState(true);
+type TOrderStatusId = {
+  params: Promise<{ id: string }>;
+};
+
+const OrderStatus = async ({ params }: TOrderStatusId) => {
+   const { id } = await params;
+   const transaction = await getTransactionById(id);
+   console.log("transaction", transaction);
 
   return (
     <main className="bg-gray-100 min-h-[80vh] pb-28">
       <div className="max-w-5xl mx-auto pt-20 pb-16">
-        <h1 className="text-5xl font-bold text-center ">Order Status</h1>
+        <h1 className="text-5xl font-bold text-center mt-24">Order Status</h1>
       </div>
-      {isConfirmed ? <OrderConfirmed /> : <OrderSubmitted />}
+      {transaction.status === "pending" && <OrderSubmitted />}
+      {transaction.status === "paid" && <OrderConfirmed />}
+      {transaction.status === "rejected" && <OrderRejected />}
     </main>
   );
 };
